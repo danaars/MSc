ae8e65ba1e6f7abb67a4e03c882f0f2cedf8ade4
danaars <daarstein@gmail.com>
2023-05-05 13:52:03 +0200

    Added 1D functionality

--- final/networks.py
+++ final/networks.py
@@ -82,3 +82,86 @@ class FlowRegClf_2D(torch.nn.Module):
         vel_reg = self._velreg(flat)
         len_reg = self._lenreg(flat)
         return class_pred, vel_reg, len_reg
+
+class FlowRegClf_1D(torch.nn.Module):
+    def __init__(self,
+                 inchannels,
+                 sample_input,
+                 device):
+        super(FlowRegClf_1D, self).__init__()
+
+        self.sample_input = sample_input
+        self.device = device
+
+        # Define activation functions
+        self.relu = torch.nn.functional.relu
+        self.sigmoid = torch.nn.functional.sigmoid
+
+        # Define convolutional kernels
+        self.conv1 = torch.nn.Conv1d(inchannels, 8, 16, device=self.device)
+        self.conv2 = torch.nn.Conv1d(8, 16, 8, device=self.device)
+        self.conv3 = torch.nn.Conv1d(16, 32, 5, device=self.device)
+        self.pool = torch.nn.MaxPool1d(2, 2)
+
+        self.flattenlen = self._getflatlen()
+
+        # Define classification layers
+        '''
+        self.classify1 = torch.nn.Linear(self.flattenlen, 128, device=self.device)
+        self.classify2 = torch.nn.Linear(128, 128, device=self.device)
+        self.classify3 = torch.nn.Linear(128, 4, device=self.device)
+        '''
+        self.classify1 = torch.nn.Linear(self.flattenlen, 64, device=self.device)
+        self.classify2 = torch.nn.Linear(64, 64, device=self.device)
+        self.classify3 = torch.nn.Linear(64, 4, device=self.device)
+
+        # Define velocity regression layers
+        self.vel1 = torch.nn.Linear(self.flattenlen, 64, device=self.device)
+        self.vel2 = torch.nn.Linear(64, 64, device=self.device)
+        self.vel3 = torch.nn.Linear(64, 1, device=self.device)
+
+        # Define length regression layers
+        self.len1 = torch.nn.Linear(self.flattenlen, 64, device=self.device)
+        self.len2 = torch.nn.Linear(64, 64, device=self.device)
+        self.len3 = torch.nn.Linear(64, 1, device=self.device)
+
+    def _getflatlen(self):
+        with torch.no_grad():
+            x = self._conv(self.sample_input)
+            l = 1
+            for i in range(len(x.shape)):
+                l *= x.shape[i]
+        return l
+
+    def _conv(self, tens):
+        x = self.pool(self.relu(self.conv1(tens)))
+        x = self.pool(self.relu(self.conv2(x)))
+        x = self.pool(self.relu(self.conv3(x)))
+        return x
+
+    def _classify(self, tens):
+        x = self.relu(self.classify1(tens))
+        x = self.relu(self.classify2(x))
+        x = self.classify3(x)
+        return x
+
+    def _velreg(self, tens):
+        x = self.relu(self.vel1(tens))
+        x = self.relu(self.vel2(x))
+        x = self.vel3(x)
+        return x
+
+    def _lenreg(self, tens):
+        x = self.relu(self.len1(tens))
+        x = self.relu(self.len2(x))
+        x = self.len3(x)
+        return x
+
+    def forward(self, tens):
+        flat = self._conv(tens)
+        flat = flat.view(-1, self.flattenlen)
+
+        class_pred = self._classify(flat)
+        vel_reg = self._velreg(flat)
+        len_reg = self._lenreg(flat)
+        return class_pred, vel_reg, len_reg
--- final/pipesound.py
+++ final/pipesound.py
@@ -3,7 +3,23 @@ import torch
 import pandas as pd
 import numpy as np
 
-def pad(tens, maxshape):
+def pad1D(tens, maxshape):
+    #Note: 1D only
+    channels, length = tens.shape
+    channels, maxlength = maxshape
+
+    len_diff = maxlength - length
+
+    lft_pad = int(np.ceil(len_diff/2))
+    rgt_pad = int(np.floor(len_diff/2))
+
+    p = (lft_pad, rgt_pad)
+    tens = torch.nn.functional.pad(tens,
+                                   pad = p,
+                                   mode='constant')
+    return tens
+
+def pad2D(tens, maxshape):
     # Note: 2D only
     channels, height, width = tens.shape
     channels, maxheight, maxwidth = maxshape
@@ -27,7 +43,8 @@ class PipeSound(torch.utils.data.Dataset):
     def __init__(self,
                  csv_file,
                  data_dir,
-                 transform = None):
+                 transform = None,
+                 dim = 1):
 
         self.data_table = pd.read_csv(os.path.join(data_dir, csv_file))
         self.data_dir = data_dir
@@ -36,6 +53,7 @@ class PipeSound(torch.utils.data.Dataset):
 
         self.lens = self.data_table['endind'] - self.data_table['startind'] + 1
         self.maxlen = self.lens.max()
+        self.dim = dim
 
     def __len__(self):
         return len(self.data_table)
@@ -55,7 +73,12 @@ class PipeSound(torch.utils.data.Dataset):
         if self.transform:
             tens = self.transform(rawdata)
 
-        tens = pad(tens, self.maxshape)
+        if self.dim == 1:
+            tens = pad1D(tens, self.maxshape)
+        elif self.dim == 2:
+            tens = pad2D(tens, self.maxshape)
+        else:
+            raise ValueError("Dim must be equal to 1 or 2, corresponding to 1D or 2D convolution")
 
         return tens, label_idx, velocity, length
 
@@ -69,4 +92,4 @@ class PipeSound(torch.utils.data.Dataset):
                       inchannels,
                       custom_transform):
         self.transform = custom_transform
-        self.maxshape = self.transform(np.ones((inchannels, self.maxlen))).shape
+        self.maxshape = self.transform(np.random.rand(inchannels, self.maxlen)).shape
